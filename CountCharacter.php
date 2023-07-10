ba4a9ac59d9ae5dd2119cad1702e5e15092e253f
andry krisnanda <andry.krisnanda@gmail.com>
2023-07-10 18:56:14 +0700

    first

---
+++ CountCharacter.php
@@ -0,0 +1,31 @@
+<?php
+
+function countCharacter($input){
+
+    if(strlen($input) > 0){
+        $arr = [];
+        for($i=0;$i<strlen($input);$i++){
+            if(isset($arr[$input[$i]])){
+                $arr[$input[$i]] += 1;
+            }else{
+                $arr[$input[$i]] = 1;
+            }
+        }
+
+        // echo json_encode($arr);
+
+        $output = '';
+        foreach($arr as $idx=>$row){
+            $output .= $idx .":".$row ." ";
+        }
+
+        echo "Input: ". $input."<br/>";
+        echo "Output: ". $output;
+    }
+}
+$a = readline('Enter an string: ');
+//'Aether'
+$char1 = countCharacter($a);
+
+
+?>
